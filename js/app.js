3ba894b8311aa6e5bc012980c475e75fdc15b230
zero9005 <177266836@qq.com>
2020-01-20 19:00:09 +0800

    search

--- js/app.js
+++ js/app.js
@@ -50,7 +50,13 @@
 			xhr.send(urlStr)
 		}
 	}
-			
+	
+	App.prototype.getQueryString = function(name) {
+		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+		var r = window.location.search.substr(1).match(reg);
+		if (r != null) return decodeURIComponent((r[2])); return null;
+	}
+	
 	w.App = new App()
 
 })(window)
